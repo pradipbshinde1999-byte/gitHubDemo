f291b7158973a52b522eae3190454dd94d73ffba
pradipbshinde1999@gmail.com <pradipbshinde1999@gmail.com>
2025-11-30 15:25:21 +0530

    INITIAL COMMIT..

---
+++ src/gitPackage/TestEX.java
@@ -0,0 +1,18 @@
+package gitPackage;
+
+public class TestEX 
+{
+	public static void main(String[] args) 
+	{
+		String hungry="yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("it's a hungry eat something");
+		}
+		else
+		{
+			System.out.println("its not a hungry");
+		}
+}
+}
